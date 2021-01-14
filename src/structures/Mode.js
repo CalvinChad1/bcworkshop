9828ed0092d7f0464c1a623cb67412d914270184
John Ng <chuchengng@gmail.com>
2021-01-14 18:21:38 +0800

    Removed activateOnStart for Mode

--- src/structures/Mode.js
+++ src/structures/Mode.js
@@ -10,15 +10,13 @@ class Mode {
      * @arg {String} name The name of the mode.
      * @arg {Function} requirement The function used to identify whether or not the mode should activate. MUST RETURN A BOOLEAN!!
      * @arg {Function} mode Stuff to do when the Bey has entered the mode. (OPTIONAL)
-     * @arg {Boolean} activateOnStart Whether the mode should already be activated / on when the battle starts. (OPTIONAL | DEFAULT = FALSE)
      */
-    constructor(name, requirement, mode, activateOnStart){
+    constructor(name, requirement, mode){
         this.name = name || "Mode",
         this.requirement = requirement || function placeholder(){
             return false;
         };
         this.mode = mode || function Modeplaceholder(){};
-        this.activateOnStart = activateOnStart || false;
     }
 }
 
