4231820902c72742d4152ea63cb13232fe96fa44
John Ng <chuchengng@gmail.com>
2021-01-14 16:45:14 +0800

    Fixed data type statements

--- src/structures/Special.js
+++ src/structures/Special.js
@@ -1,15 +1,15 @@
 /**
  * @prop {String} name The name of the special move.
- * @prop {function} requirement The function used to define whether the special is executable.
- * @prop {function} special The actual special move to be executed.
+ * @prop {Function} requirement The function used to define whether the special is executable.
+ * @prop {Function} special The actual special move to be executed.
  */
 
 class Special {
     /**
      * 
      * @arg {String} name The name of the special move. 
-     * @arg {function} requirement The function used to define whether the special is executable. MUST RETURN A BOOLEAN!
-     * @arg {function} special The actual special move to be executed.
+     * @arg {Function} requirement The function used to define whether the special is executable. MUST RETURN A BOOLEAN!
+     * @arg {Function} special The actual special move to be executed.
      */
     constructor(name, requirement, special){
         this.name = name || "Special",
